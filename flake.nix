98f118679561a97240639c0347f341f2556da48f
Ethan Zimbelman <ethan.zimbelman@me.com>
2024-02-29 21:28:36 -0800

    build(tofu): prepare a tool to configure infrastructure as code

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "a collection of API methods";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.opentofu
+          ];
+        };
+      });
+}
